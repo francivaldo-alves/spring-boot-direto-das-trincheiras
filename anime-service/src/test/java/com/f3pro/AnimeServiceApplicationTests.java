bfb3ea639387aad070802194ee369d0280aa449f
Francivaldo Alves <francivaldo.alves@gmail.com>
2024-12-07 17:20:23 -0300

    video03: initial commit of the project

---
+++ anime-service/src/test/java/com/f3pro/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f3pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
